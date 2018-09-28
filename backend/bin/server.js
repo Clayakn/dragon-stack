8f364dd293fcc290cb460e94e35a891bda7d8402
Andrew <chicken.noodlesoup99@gmail.com>
2018-09-28 11:49:58 -0700

    Add api folder

--- backend/bin/server.js
+++ backend/bin/server.js
@@ -1,4 +1,5 @@
 const app = require('../app');
 
 const PORT = 4001
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)});
